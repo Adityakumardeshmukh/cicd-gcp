47b8d08d0d96f585c8f7588000d1778dbe5cb105
Adityakumardeshmukh <deshmukhaditya810@gmail.com>
2024-06-07 16:22:43 +0530

    testing cd

--- terraform/modules/vendor/main.tf
+++ terraform/modules/vendor/main.tf
@@ -37,27 +37,40 @@ resource "google_storage_bucket_object" "my_object" {
   source = data.archive_file.function_zip.output_path
 }
 
-# # Cloud Function
-# resource "google_cloudfunctions_function" "vendor_function" {
-#   name        = "vendor-function"
-#   runtime     = "python39"
-#   entry_point = "hello_pubsub"
-#   source_archive_bucket = google_storage_bucket.vendor_bucket.name
-#   source_archive_object = google_storage_bucket_object.function_source.name
-#   project               = var.project_id
-#   service_account_email = "cicd-gcp-appengine@cicd-gcp-424408.iam.gserviceaccount.com"
+# BigQuery Dataset
+resource "google_bigquery_dataset" "my_dataset" {
+  dataset_id = "db_demo_1"
+  project    = var.project_id
+  location   = var.region
+}
 
-#   environment_variables = {
-#     project_id    = var.project_id
-#     dataset_id    = "my_dataset"  # Replace with your dataset
-#     table_id      = "my_table"    # Replace with your table
-#   }
+# BigQuery Table
+resource "google_bigquery_table" "my_table" {
+  dataset_id = google_bigquery_dataset.my_dataset.dataset_id
+  table_id   = "table_demo_1"
+  project    = var.project_id
+}
+# Cloud Function
+resource "google_cloudfunctions_function" "vendor_function" {
+  name        = "function_demo_1"
+  runtime     = "python39"
+  entry_point = "hello_pubsub"
+  source_archive_bucket = google_storage_bucket.vendor_bucket.name
+  source_archive_object = google_storage_bucket_object.my_object.name
+  project               = var.project_id
+  service_account_email = "cicd-gcp-appengine@cicd-gcp-424408.iam.gserviceaccount.com"
 
-#   event_trigger {
-#     event_type = "google.pubsub.topic.publish"
-#     resource   = google_pubsub_topic.vendor_topic.name
-#   }
-# }
+  environment_variables = {
+    project_id    = var.project_id
+    dataset_id    = google_bigquery_dataset.my_dataset.dataset_id  # Replace with your dataset
+    table_id      = google_bigquery_table.my_table.table_id    # Replace with your table
+  }
+
+  event_trigger {
+    event_type = "google.pubsub.topic.publish"
+    resource   = google_pubsub_topic.vendor_topic.name
+  }
+}
 
 # resource "google_project_iam_binding" "cloudfunctions_invoker" {
 #   project = var.project_id
